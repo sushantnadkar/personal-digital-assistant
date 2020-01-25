d60ab146a27de59da6eaa9a8cf1cc2324649146d
Sushant Nadkar <nadkarsushant@gmail.com>
2020-01-25 16:34:05 +0530

    cli interface added by implimenting argparse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import yaml
+import argparse
 import speech_recognition as sr
 
 from brain import brain
@@ -10,22 +11,30 @@ profile = open("profile.yaml")
 profile_data = yaml.safe_load(profile)
 profile.close()
 
-speak("Welcome" + name + ", systems are now ready to run. How may i help you?")
+speak("Welcome" + profile_data["name"] + ", systems are now ready to run. How may i help you?")
 
 def main():
-	r = sr.Recognizer() 
-	with sr.Microphone() as source:
-		print("Say something!")
-		audio = r.listen(source)
 
-	try:
-		speech_text = r.recognize_google(audio).lower().replace("'", "")
-		print("Google thinks you said: " + speech_text)
-	except sr.UnknownValueError:
-		print("Google could not understand audio")
-	except sr.RequestError as e:
-		print("Google error; {0}".format(e))
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-t", "--text", help="Provide text input instade of speech input using microphone.")
+	args = parser.parse_args()
 
-	brain(speech_text, **kwargs)
+	if args.text:
+		speech_text = args.text
+	else:
+		r = sr.Recognizer()
+		with sr.Microphone() as source:
+			print("Say something!")
+			audio = r.listen(source)
+
+		try:
+			speech_text = r.recognize_google(audio).lower().replace("'", "")
+			print("Google thinks you said: " + speech_text)
+		except sr.UnknownValueError:
+			print("Google could not understand audio")
+		except sr.RequestError as e:
+			print("Google error; {0}".format(e))
+
+	brain(speech_text, **profile_data)
 
 main()
